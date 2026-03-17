bd12aabda75ae9b1731988100ca76cc54c7a5f84
Gianmarco Mejía <gianmarcomejia@gmail.com>
2026-03-16 22:40:40 -0500

    fix session path

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
-SESSION_FILE = os.path.join(BASE_DIR, "session.json")
+SESSION_FILE = "/tmp/session.json"
 
 
 # =========================
